086790c974783452187390ff2aaa8d46956e2cc4
Derin Theiventhiram <derin.theiventhiram@stud.uni-heidelberg.de>
2024-03-11 14:59:29 +0100

    remove unused library

--- autoencoders.py
+++ autoencoders.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from typing import *
-from torchvision.datasets import ImageFolder
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
