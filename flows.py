0bbfe0747df8c361a9853256d3077ba0b237d30b
Daniel Hammer <d.h.99@web.de>
2024-03-20 17:22:23 +0100

    move into lightinig class

--- flows.py
+++ flows.py
@@ -1,5 +1,6 @@
 import torch
 import lightning as L
+from autoencoders import LightningVAE
 
 from typing import Any
 
@@ -92,10 +93,11 @@ class AffineCouplingLayer(torch.nn.Module):
         z = torch.hstack([z_upper, z_lower])
 
         return z
-    
-class RealNVP(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, blocks, condition_size = 0, coupling_layer = "Affine", printing = False):
-        super(RealNVP, self).__init__()
+
+
+class LightningRealNVP(L.LightningModule):
+    def __init__(self, input_size: int, hidden_size: int, blocks: int, condition_size: int = 0, lr=1e-3, autoencoder: LightningVAE = None):
+        super().__init__()
 
         assert(hidden_size > input_size)
 
@@ -103,54 +105,49 @@ class RealNVP(torch.nn.Module):
         self.hidden_size = hidden_size
         self.blocks = blocks
         self.condition_size = condition_size
+        self.lr = lr
 
-        if coupling_layer=="Affine":
-            self.coupling_blocks = torch.nn.ModuleList([AffineCouplingLayer(self.input_size,self.hidden_size, self.condition_size) for i in range(self.blocks-1)])
-            self.coupling_blocks.append(AffineCouplingLayer(self.input_size, self.hidden_size, self.condition_size, rotate=False))
+        self.autoencoder = autoencoder
+        self.autoencoder.requires_grad_(False)
 
-        if printing == True:
-            print("RealNVP initialization: -----------------------------------")
-            print(f"input_size: {input_size}")
-            print(f"hidden_size: {hidden_size}")
-            print(f"coupling blocks: {self.coupling_blocks}")
-            print("------------------------------------------------------------")
+        self.save_hyperparameters(ignore=['autoencoder'])
 
-        self.Qs = torch.linalg.qr(torch.randn((self.blocks-1, self.input_size, self.input_size)))[0]
 
-    def forward(self, x, conditions = None):
+        self.coupling_blocks = torch.nn.ModuleList([AffineCouplingLayer(self.input_size,self.hidden_size, self.condition_size) for i in range(self.blocks-1)])
+        self.coupling_blocks.append(AffineCouplingLayer(self.input_size, self.hidden_size, self.condition_size, rotate=False))
+
+
+    def forward(self, x, conditions=None):
         for i, coupling_layer in enumerate(self.coupling_blocks):
             x = coupling_layer.forward(x, conditions)
             
         return x 
     
-    def inverse(self, x, conditions = None):
+    def inverse(self, z, conditions=None):
         for i in range(self.blocks):
-            x = self.coupling_blocks[self.blocks-1-i].inverse(x, conditions)
+            z = self.coupling_blocks[self.blocks-1-i].inverse(z, conditions)
 
-        return x
+        return z
 
-    def sample(self, num_samples, conditions):
+    def sample(self, num_samples, conditions=None):
         normal_samples = torch.randn((num_samples, self.input_size))
             
         samples = self.inverse(normal_samples, conditions)
         
         return samples
-
-class LightningINN(L.LightningModule):
-    def __init__(self, inn: RealNVP, encoder):
-        super().__init__()
-
-        self.inn = inn
-        self.encoder = encoder
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.coupling_blocks.parameters(), lr=self.lr)
+        return optimizer
 
     def training_step(self, batch):
         x, conditions = batch    
-        conditions = self.encoder(conditions)[0]  
+        conditions = self.autoencoder.encode(conditions)
                             
         size = x.shape[0]
         z = x
         log_det_jac = 0
-        for block in self.inn.coupling_blocks:
+        for block in self.coupling_blocks:
             log_det_jac += torch.sum(block.get_transforms(z[:, :block.upper], conditions)[0])
             z = block(z, conditions)
             
@@ -160,8 +157,22 @@ class LightningINN(L.LightningModule):
         self.log("train_loss", loss)
 
         return loss
+    
+    def validation_step(self, batch) -> torch.Tensor:
+        x, conditions = batch    
+        conditions = self.autoencoder.encode(conditions)
+                            
+        size = x.shape[0]
+        z = x
+        log_det_jac = 0
+        for block in self.coupling_blocks:
+            log_det_jac += torch.sum(block.get_transforms(z[:, :block.upper], conditions)[0])
+            z = block(z, conditions)
+            
+        loss = ( 1 / size ) * ( ( torch.sum(torch.square(z)) / 2 ) - log_det_jac )
+        
+        # Logging to TensorBoard (if installed) by default
+        self.log("val_loss", loss)
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+        return loss  
 
